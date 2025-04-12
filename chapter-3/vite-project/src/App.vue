ecabd8bcefec87f1362da3c0f5ae5b89cbf4996b
butchi <butchiyu+com-github@gmail.com>
2025-04-12 20:11:37 +0900

    サンプル追加

---
+++ chapter-3/vite-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue'
+
+const exampleMessage = 'Hello, world!'
+const textColor = '#ff0000'
+</script>
+
+<template>
+  <h1>Vueサンプル</h1>
+
+  <h2>コンポーネントの例</h2>
+  <ButtonCounter />
+
+  <h2>データバインディングの例</h2>
+  <p :style="{ color: textColor }">{{ exampleMessage }}</p>
+</template>
---
+++ chapter-3/vite-project/src/_App-component.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue'
+</script>
+
+<template>
+  <h1>Vueサンプル</h1>
+
+  <h2>コンポーネントの例</h2>
+  <ButtonCounter />
+</template>
