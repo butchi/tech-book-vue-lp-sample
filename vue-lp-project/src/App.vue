8d0a3e5c2c04de82004b271c8726590fdc91c899
butchi <butchiyu+com-github@gmail.com>
2025-04-12 17:56:53 +0900

    テストページ作成

---
+++ vue-lp-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary">
+      <v-app-bar-title>テストページへようこそ</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-title>こんにちは</v-card-title>
+              <v-card-text>Hello, world!</v-card-text>
+              <v-card-actions>
+                <v-btn
+                  color="primary"
+                  variant="elevated"
+                  href="https://www.example.com"
+                >
+                  ボタン
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
