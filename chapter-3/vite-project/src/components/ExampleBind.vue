53e96a4f0e0504f0e8cfd5f5137796db26cffd46
butchi <butchiyu+com-github@gmail.com>
2025-04-14 19:10:42 +0900

    データバインディングの例を修正

--- chapter-3/vite-project/src/components/ExampleBind.vue
+++ chapter-3/vite-project/src/components/ExampleBind.vue
@@ -1,11 +1,11 @@
 <script setup>
 const exampleMessage = 'Hello, world!'
-const textColor = '#ff0000'
+const exampleStyle = { color: '#ff0000' }
 </script>
 
 <template>
   <v-container>
     <h2>データバインディングの例</h2>
-    <p :style="{ color: textColor }">{{ exampleMessage }}</p>
+    <p :style="exampleStyle">{{ exampleMessage }}</p>
   </v-container>
 </template>
